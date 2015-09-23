fa5047cdb3753d3d6d11188a9a700f49fc67d0bc
noncreativetype <frpatino@gmail.com>
2015-09-23 17:05:01 -0400

    Update TwilioServlet.java

--- TwilioServlet.java
+++ TwilioServlet.java
@@ -3,7 +3,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
  
 import com.twilio.sdk.verbs.TwiMLResponse;
 import com.twilio.sdk.verbs.TwiMLException;
@@ -11,29 +10,13 @@ import com.twilio.sdk.verbs.Message;
  
 public class TwilioServlet extends HttpServlet {
  
+    // service() responds to both GET and POST requests.
+    // You can also use doGet() or doPost()
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Create a dict of people we know.
-        HashMap<String, String> callers = new HashMap<String, String>();
-        callers.put("+14158675309", "Curious George");
-        callers.put("+14158675310", "Boots");
-        callers.put("+14158675311", "Virgil");
- 
-        String fromNumber = request.getParameter("From");
-        String knownCaller = callers.get(fromNumber);
-        String message;
-        if (knownCaller == null) {
-            // Use a generic message
-            message = "Monkey, thanks for the message!";
-        } else {
-            // Use the caller's name
-            message = knownCaller + ", thanks for the message!";
-        }
- 
-        // Create a TwiML response and add our friendly message.
         TwiMLResponse twiml = new TwiMLResponse();
-        Message sms = new Message(message);
+        Message message = new Message("Hello, Mobile Monkey");
         try {
-            twiml.append(sms);
+            twiml.append(message);
         } catch (TwiMLException e) {
             e.printStackTrace();
         }
